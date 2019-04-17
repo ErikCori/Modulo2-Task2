1ec004ba196c2ea5c5d051bbf229b56aa0601911
Diego Weinmann <diego.weinmann@hotmail.com>
2019-04-17 12:20:58 -0300

    task2 punto5 completa

--- Senado_FullStack/senate-carga.js
+++ Senado_FullStack/senate-carga.js
@@ -1,33 +1,48 @@
 //document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
 
-var resultados = data.results[0].members;
-var cantMiembros= data.results[0].num_results;
-var elementosTabla=[];
+if(document.getElementById('senate-data')){
+	createSenatorsTable();
+}else if(document.getElementById('house-data')){
+	createHouseTable();
+}
 
-for(var i=0; i<cantMiembros;i++){
-	if(resultados[i].middle_name != null){
-	var trCompleto = resultados[i].first_name+" "+resultados[i].middle_name+" "+resultados[i].last_name+" "+resultados[i].party+" "+resultados[i].state+" "+resultados[i].seniority+" %"+resultados[i].votes_with_party_pct;
-	}else{
-		var trCompleto = resultados[i].first_name+" "+resultados[i].last_name+" "+resultados[i].party+" "+resultados[i].state+" "+resultados[i].seniority+" %"+resultados[i].votes_with_party_pct;
-	}
-	console.log(trCompleto);
+createSenatorsTable();
+
+function createSenatorsTable(){
+	var tableFinal = addTableToHtml(data.results[0].members);
+	var tableSenators = document.getElementById('senate-data');
+	tableSenators.innerHTML = tableFinal;
+}
+function createHouseTable(){
+	var tableFinal = addTableToHtml(data.results[0].members);
+	var tableHouse = document.getElementById('house-data');
+	tableHouse.innerHTML = tableFinal;
 }
 
 
+function addTableToHtml(members){
+	var table = '<thead class="thead"><tr><th>Full name</th><th>Party</th><th>State</th><th>Seniority</th><th>Percentage of votes whith party</th></tr></thead>';
+	
+	table += '<tbody>';
+	
+	members.forEach(function(member){
+		table += '<tr>';
+		if(member.middle_name === null){
+			table += '<td ><a href ="' +member.url+ '">' + member.first_name+' '+member.last_name+'</td>';
+		}else{
+			table +='<td><a href="' +member.url+ '">'+member.first_name+' '+member.middle_name+' '+member.last_name+'</td>';
+		}
+	table += '<td class="party">'+member.party+'</td>';
+	table += '<td class="state">'+member.state+'</td>';
+	table += '<td>'+member.seniority+'</td>';
+	table += '<td>% '+member.votes_with_party_pct+'</td>';
+	table += '</tr>';
+	})
+	table += '</tbody>';
+	return table;
+}
+									
 
-/*resultados.forEach(elemento =>agregarTR(crearTRStringTabla(elemento), elementosTabla));
-console.log(elementosTabla);
-function crearTRStringTabla(elemento){
-	return elemento.first_name +" "+elemento.middle_name+" "+elemento.last_name+" "+elemento.party+" "+elemento.state+" "+elemento.seniority+" %"+elemento.votes_with_party_pct;
-}
-function agregarTR(elemento, cadenaAcumulador){
-	cadenaAcumulador +=elemento;
-}
-function crearElementosTabla(){
-	resultados.forEach(elemento => elementosTabla = elemento.first_name +" "+elemento.middle_name+" "+elemento.last_name+" "+
-										elemento.party+" "+elemento.state+" "+elemento.seniority+" %"+elemento.votes_with_party_pct
-										)
-	return resultados;
-}*/
+
 
 
